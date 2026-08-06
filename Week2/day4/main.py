de4e9d7ef41bacb3213f5584650683c28f774c8c
Utkarsh Agarwal <utkarshagarwal1299@gmail.com>
2026-08-06 22:54:43 +0530

    Stream
    Printing Chunk by chunk as and when it gets generated instead of printing the whole message at once

---
+++ Week2/day4/main.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+print(f"API Key: {GROQ_API_KEY}")
+
+if not GROQ_API_KEY:
+    raise ValueError("GROQ_API_KEY is not set in the environment variables.")
+
+client = Groq(api_key=GROQ_API_KEY)
+model="llama-3.3-70b-versatile"
+message = "Hello, Groq! Can you tell me about moon? Answer in 500 words"
+role = "user"
+
+stream = client.chat.completions.create(
+    model=model,
+    messages=[{"role":role, "content": message}],
+    stream=True # by default it is false - true when the answer is read to user.
+)
+
+print("**********Message Content**********")
+# Stream - instead of printing the entire result in one go, if we want to print the line as soon as it gets generated we use stream. it is used to increase user experience and reduce the response time
+for chunk in stream:
+    if chunk.choices:
+        print(chunk.choices[0].delta.content, end="", flush=True)
